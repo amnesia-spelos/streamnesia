aab095ac7935647a2d735be98592ab447e624fa7
petrspelos <petrspelos@users.noreply.github.com>
2025-04-24 16:47:29 +0200

    Implement Poll<T>

---
+++ src/Streamnesia.Core/IPoll.cs
@@ -0,0 +1,16 @@
+﻿using FluentResults;
+
+namespace Streamnesia.Core;
+
+public interface IPoll<TItem> where TItem : class
+{
+    IReadOnlyCollection<TItem>? Options { get; }
+
+    void SetOptions(IEnumerable<TItem> options);
+
+    Result SetNamedVote(string name, int optionIndex);
+
+    void ClearAllVotes();
+
+    TItem GetVotedItem();
+}
