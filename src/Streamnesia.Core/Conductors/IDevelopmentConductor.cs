27898fdfc7208d03412452fbc4e3e4ab32aadd07
petrspelos <petrspelos@users.noreply.github.com>
2025-05-06 15:12:11 +0200

    Implement development conductor

---
+++ src/Streamnesia.Core/Conductors/IDevelopmentConductor.cs
@@ -0,0 +1,8 @@
+﻿namespace Streamnesia.Core.Conductors;
+
+public interface IDevelopmentConductor
+{
+    event Func<string, CancellationToken, Task>? OnErrorAsync;
+
+    Task ExecuteAsync(CancellationToken cancellationToken = default);
+}
